a8eac233fcfd043eb78bb94f04f7349329029067
ad <wxing374@gmail.com>
2026-08-10 09:03:59 -0700

    add parsetree

--- nodejs/udp-test.js
+++ nodejs/udp-test.js
@@ -1,3 +1,12 @@
 //获取udp模块
 let dgram = require('dgram')
+//创建
 let socket = dgram.createSocket('udp4')
+
+socket.bind(52345)
+//发送
+socket.send('wwwwwwwww', 10010, '127.0.0.1')
+
+socket.on('message', (data, info) => {
+
+})
